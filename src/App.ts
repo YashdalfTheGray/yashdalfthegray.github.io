4ee59acee264f9e70fa7996f36da1827c79d071b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-07-09 08:02:41 -0700

    added some styles and the new component

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,13 @@
 import IComponent from './Component';
+import Header from './Header';
 
 export default class App implements IComponent {
     public render() {
+        const header = new Header();
         return `
-            <h1 class="page-heading">Yash Kulshrestha</h1>
+            <div class="app-container">
+                ${header.render()}
+            </div>
         `;
     }
 }
