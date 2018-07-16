13dc8e5955620a37a0683d5dc7374e36b33cd8fb
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-07-16 07:39:20 -0600

    added picture on the hero banner

--- src/HeroBanner.ts
+++ src/HeroBanner.ts
@@ -3,8 +3,9 @@ import Component from './Component';
 export default class HeroBanner implements Component {
     public render() {
         return `
-            <div class="app-content">
-                <img src="../assets/header.jpg" />
+            <div class="hero-banner">
+                <img class="hero-background" src="../assets/header.jpg" />
+                <img class="mugshot" src="https://avatars1.githubusercontent.com/u/3102848?v=4" />
             </div>
         `;
     }
