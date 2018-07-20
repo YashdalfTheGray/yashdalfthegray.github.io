80076eb9b5335b7bcfc804c985db43714a914271
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-07-20 09:04:45 -0700

    switched component from interface to class with component registry

--- src/Component.ts
+++ src/Component.ts
@@ -1,3 +1,20 @@
-export default interface IComponent {
-    render: (props?: object) => string;
+interface IDocumentWithRegistry extends Document {
+    componentRegistry: object;
+    nextId: number;
+}
+
+(document as IDocumentWithRegistry).componentRegistry = {};
+(document as IDocumentWithRegistry).nextId = 0;
+
+export default class Component {
+    private id: string;
+
+    constructor(type: string = 'Component') {
+        this.id = `${type}${++(document as IDocumentWithRegistry).nextId}`;
+        (document as IDocumentWithRegistry).componentRegistry[this.id] = this;
+    }
+
+    public render(props?: object): string {
+        return '';
+    }
 }
