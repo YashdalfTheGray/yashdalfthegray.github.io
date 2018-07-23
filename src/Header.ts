da90ca2c04559940a917fb716dd40e7afdf85682
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-07-23 07:45:44 -0700

    added a menu that collapses but need to animate

--- src/Header.ts
+++ src/Header.ts
@@ -8,31 +8,36 @@ export default class Header extends Component {
 
     public handleMenuArrowClick() {
         const arrowDiv = document.querySelector('.header .anchor-menu-button');
+        const anchorMenu = document.querySelector('.anchor-menu');
 
         if (arrowDiv.classList.contains('activated')) {
             arrowDiv.classList.remove('activated');
+            anchorMenu.classList.remove('visible');
         }
         else {
             arrowDiv.classList.add('activated');
+            anchorMenu.classList.add('visible');
         }
     }
 
     public render() {
+        const anchors = ['About', 'Projects', 'Contact'];
         const arrowDown = new ArrowDown();
 
         return `
             <div class="header flex-row">
                 <span class="title">Yash Kulshrestha</span>
                 <div class="flex-row h-list anchor-bar">
-                    <span>About</span>
-                    <span>Projects</span>
-                    <span>Contact</span>
+                    ${anchors.map(a => `<span>${a}</span>`).join('\n')}
                 </div>
                 <div
                     class="anchor-menu-button"
                     onClick="document.componentRegistry['${this.id}'].handleMenuArrowClick()">
                     ${arrowDown.render()}
                 </div>
+            </div>
+            <div class="anchor-menu flex-column">
+                ${anchors.map(a => `<span>${a}</span>`).join('\n')}
             </div>
         `;
     }
