50eb068813fdab28cbbb334ad3975976f33838c5
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-07-25 08:08:23 -0700

    added a projects section and added a title to the about section

--- src/About.ts
+++ src/About.ts
@@ -4,7 +4,8 @@ export default class About extends Component {
     public render() {
         /* tslint:disable:max-line-length */
         return `
-            <div name="anchor_about" class="about-section">
+            <h2 class="section-heading" name="anchor_about">About</h2>
+            <div class="about-section">
                 <p>Hi! I'm Yash, a Buckeye living in Seattle.</p>
                 <p>I maintain an AngularJS webapp by day, experiment and learn through open-source contributions by night. React, Vue.js, Node.js, Typescript, Express.js, Webpack, Javascript testing and UI testing are all areas of expertise. Docker, container orchestration, Go, Ruby, Rails, Sinatra and Elm are all areas of interest.</p>
                 <p>I read, game and stream, tinker with electronics and keep up with Ohio State Football as much as I can in my free time.</p>
---
+++ src/Projects.ts
@@ -0,0 +1,9 @@
+import Component from './Component';
+
+export default class Projects extends Component {
+    public render() {
+        return `
+            <div class="projects-section"></div>
+        `;
+    }
+}
