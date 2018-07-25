d133c534f8e805337fcef3b08d835a07fd84d899
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-07-25 08:08:36 -0700

    included the projects section on the page

--- src/App.ts
+++ src/App.ts
@@ -3,6 +3,7 @@ import Component from './Component';
 import About from './About';
 import Header from './Header';
 import HeroBanner from './HeroBanner';
+import Projects from './Projects';
 
 export default class App extends Component {
     constructor() {
@@ -13,12 +14,14 @@ export default class App extends Component {
         const header = new Header();
         const heroBanner = new HeroBanner();
         const about = new About();
+        const projects = new Projects();
 
         return `
             <div class="flex-column">
                 ${header.render()}
                 ${heroBanner.render()}
                 ${about.render()}
+                ${projects.render()}
             </div>
         `;
     }
