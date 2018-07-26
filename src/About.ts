4d8c31e4d67ba5c5799ae25832d02219e09778ab
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-07-26 08:52:50 -0700

    fixed the anchor links and added some styles

--- src/About.ts
+++ src/About.ts
@@ -4,7 +4,9 @@ export default class About extends Component {
     public render() {
         /* tslint:disable:max-line-length */
         return `
-            <h2 class="section-heading" name="anchor_about">About</h2>
+            <h2 class="section-heading">
+                <a class="anchor-link" name="about">About</a>
+            </h2>
             <div class="about-section">
                 <p>Hi! I'm Yash, a Buckeye living in Seattle.</p>
                 <p>I maintain an AngularJS webapp by day, experiment and learn through open-source contributions by night. React, Vue.js, Node.js, Typescript, Express.js, Webpack, Javascript testing and UI testing are all areas of expertise. Docker, container orchestration, Go, Ruby, Rails, Sinatra and Elm are all areas of interest.</p>
--- src/Projects.ts
+++ src/Projects.ts
@@ -3,7 +3,12 @@ import Component from './Component';
 export default class Projects extends Component {
     public render() {
         return `
-            <div class="projects-section"></div>
+            <h2 class="section-heading">
+                <a class="anchor-link" name="projects">Projects</a>
+            </h2>
+            <div class="projects-section">
+                <p>These are some of my pet projects on Github!</p>
+            </div>
         `;
     }
 }
