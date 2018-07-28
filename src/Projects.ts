25e8d17370208ddf340b5fd6cd8840b9e5b7a35e
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-07-28 13:13:14 -0700

    added loading indicator to projects and some styles

--- src/Projects.ts
+++ src/Projects.ts
@@ -1,17 +1,34 @@
 import Component from './Component';
 
+import Loading from './Loading';
+
 export default class Projects extends Component {
-    public render() {
+    public renderProjects(projects) {
         return `
-            <h2 class="section-heading">
-                <a class="anchor-link" name="projects">Projects</a>
-            </h2>
+            <div class="projects-section">
+                <p>These are some of my pet projects on Github!</p>
+            </div>
+        `;
+    }
+
+    public renderFallback() {
+        return `
             <div class="projects-fallback">
                 You can find my active pet projects on my&nbsp;
                 <a href="https://github.com/YashdalfTheGray">Github page</a>!
             </div>
-            <div class="projects-section">
-                <p>These are some of my pet projects on Github!</p>
+        `;
+    }
+
+    public render() {
+        const loading = new Loading();
+
+        return `
+            <h2 class="section-heading">
+                <a class="anchor-link" name="projects">Projects</a>
+            </h2>
+            <div class="projects-container">
+                ${loading.render()}
             </div>
         `;
     }
