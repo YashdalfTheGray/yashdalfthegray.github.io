3546686aa68022a81ac6ab88ec94dacaf9357840
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-07-29 14:34:43 -0700

    added a list of projects to the app state

--- src/App.ts
+++ src/App.ts
@@ -5,16 +5,32 @@ import Header from './Header';
 import HeroBanner from './HeroBanner';
 import Projects from './Projects';
 
+interface IAppState {
+    projectsList: string[];
+}
+
 export default class App extends Component {
+    public state: IAppState;
+
     constructor() {
         super('app');
+        this.state = {
+            projectsList: [
+                'hue-remote',
+                'onboard',
+                'light-control',
+                'docker-dashboard',
+                'auto-ngtemplate-loader',
+                'cluster-cleanup'
+            ]
+        };
     }
 
     public render() {
         const header = new Header();
         const heroBanner = new HeroBanner();
         const about = new About();
-        const projects = new Projects();
+        const projects = new Projects(this.state.projectsList);
 
         return `
             <div class="flex-column">
