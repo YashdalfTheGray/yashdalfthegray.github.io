933793034a938cd4e5bfad84574733fe96f72437
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-07-30 08:20:09 -0700

    load the projects and remove the spinner

--- src/Projects.ts
+++ src/Projects.ts
@@ -2,9 +2,37 @@ import Component from './Component';
 
 import Loading from './Loading';
 
+interface IProjectState {
+    projectDetails: object[];
+}
+
 export default class Projects extends Component {
+    public state: IProjectState;
+
     constructor(projects: string[]) {
         super('projects');
+        this.state = {
+            projectDetails: []
+        };
+
+        setTimeout(() => this.getProjectDetails.bind(this, projects)());
+    }
+
+    public async getProjectDetails(projects: string[]) {
+        const projectsContainer = document.querySelector('div.projects-container');
+
+        if (!window.fetch) {
+            projectsContainer.innerHTML = this.renderFallback();
+            return;
+        }
+        else {
+            this.state.projectDetails = await Promise.all(projects.map(async (p) => {
+                const response = await fetch(`https://api.github.com/repos/yashdalfthegray/${p}`);
+                return response.json();
+            }));
+
+            projectsContainer.innerHTML = this.renderProjects(this.state.projectDetails);
+        }
     }
 
     public renderProjects(projects) {
