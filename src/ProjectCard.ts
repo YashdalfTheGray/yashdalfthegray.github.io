785f5f33f3667848b42c25f0bed91d2a62e9daa7
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-08-03 08:32:10 -0700

    basic cards with links and shit

--- src/ProjectCard.ts
+++ src/ProjectCard.ts
@@ -3,14 +3,17 @@ import Component from './Component';
 import { IGithubRepo } from './types';
 
 export default class ProjectCard extends Component {
-    constructor() {
+constructor() {
         super('projectcard');
     }
 
     public render(props: IGithubRepo) {
+        const { id, name, html_url } = props;
         return `
-            <div class="project-card" id="project-card-${props.id}">
-                <span>${props.name}</span>
+            <div class="project-card" id="project-card-${id}">
+                <a href="${html_url}" target="_blank">
+                    <span>${name}</span>
+                </a>
             </div>
         `;
     }
