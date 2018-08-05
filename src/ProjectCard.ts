7d264f1f8383d5413c13c664e5809bdebd807ad5
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-08-05 07:37:51 -0700

    added some style to the list to differentiate

--- src/ProjectCard.ts
+++ src/ProjectCard.ts
@@ -11,7 +11,7 @@ constructor() {
         const { id, name, description, html_url, language } = props;
         return `
             <div class="project-card" id="project-card-${id}">
-                <a href="${html_url}" target="_blank" class="v-list">
+                <a href="${html_url}" target="_blank">
                     <span class="card-heading">${name}</span>
                     <span class="card-description">${description}</span>
                     <span class="card-language">${language}</span>
