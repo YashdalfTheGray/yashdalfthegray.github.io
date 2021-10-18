75740174c5cfa9fd4d1fb797e05341a05fd517d7
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-10-18 17:06:59 -0400

    added a test file that will serve as a canary

---
+++ test/canary.spec.ts
@@ -0,0 +1,10 @@
+import test from 'ava';
+import { withPage } from './utils/withPage';
+
+test.serial('canary', withPage, async (t, page) => {
+  await page.goto(process.env.WEBSITE_URL);
+  const contactsListElements = await page.$$('.contacts-list span');
+
+  t.is(await page.title(), 'Yash Kulshrestha');
+  t.is(contactsListElements.length, 7);
+});
