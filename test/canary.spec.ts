a94a846c96fce7284fcf2c3f390703357a4f1096
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-10-18 17:19:05 -0400

    added a snapshot to the canary test

--- test/canary.spec.ts
+++ test/canary.spec.ts
@@ -5,6 +5,8 @@ test.serial('canary', withPage, async (t, page) => {
   await page.goto(process.env.WEBSITE_URL);
   const contactsListElements = await page.$$('.contacts-list span');
 
+  t.snapshot(await page.content());
+
   t.is(await page.title(), 'Yash Kulshrestha');
   t.is(contactsListElements.length, 7);
 });
