064f4af71847e544a5d6d4ed179fc9913aa1c00a
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-10-21 14:00:20 -0400

    added the request interception right on the page object in the helper

--- test/utils/withPage.ts
+++ test/utils/withPage.ts
@@ -1,6 +1,8 @@
 import * as puppeteer from 'puppeteer';
 import { ExecutionContext, ImplementationResult } from 'ava';
 
+import { setupGithubRequestInterception } from './requestInterception';
+
 export type ImplementationWithPage<T> = (
   t: ExecutionContext<T>,
   page: puppeteer.Page
@@ -12,9 +14,14 @@ export async function withPage<T>(
 ) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
+  await page.setRequestInterception(true);
+  const unsubscribe = await setupGithubRequestInterception(page);
+
   try {
     await run(t, page);
   } finally {
+    unsubscribe();
     await page.close();
     await browser.close();
   }
